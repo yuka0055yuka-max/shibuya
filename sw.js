2df7ed0f0950831a2f83d199b180f1be2b92f199
yuka0055yuka-max <yuka0055yuka@gmail.com>
2025-11-04 01:28:51 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'scramble-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './style.css',
+  './script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
